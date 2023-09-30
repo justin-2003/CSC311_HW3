187530a6c4610e47a66d66ca348b61b5d1b23f9a
karij5 <karij5@farmingdale.edu>
2023-09-30 18:31:50 -0400

    added robot class (unsure if needed), added buttons linked to the a maze running method and a mapswap method, linked controller with FXML, added robot and buttons for mapswap and maze running method now shown, the window will not be resizable

--- src/main/java/com/example/csc311_hw3/MazeController.java
+++ src/main/java/com/example/csc311_hw3/MazeController.java
@@ -2,13 +2,19 @@ package com.example.csc311_hw3;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 
 public class MazeController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void runMaze() {
+
+    }
+
+    @FXML
+    protected void mapSwap() {
+
     }
 }
--- src/main/java/com/example/csc311_hw3/Robot.java
+++ src/main/java/com/example/csc311_hw3/Robot.java
@@ -1,4 +1,21 @@
 package com.example.csc311_hw3;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class Robot {
+
+    @FXML
+    ImageView img_view;
+
+
+
+    public Robot(){
+
+    }
+
+    public Robot(ImageView img){
+
+    }
+
 }
